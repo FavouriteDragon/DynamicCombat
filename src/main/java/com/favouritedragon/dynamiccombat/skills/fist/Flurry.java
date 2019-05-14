f9b6c9932b2f4300d80637a052d5fc9d8fc72009
FavouriteDragon <ninjadragon008@icloud.com>
2019-05-14 16:10:43 -0700

    Trying to get my dependencies to work o_0

--- src/main/java/com/favouritedragon/dynamiccombat/skills/fist/Flurry.java
+++ src/main/java/com/favouritedragon/dynamiccombat/skills/fist/Flurry.java
@@ -1,11 +1,19 @@
 package com.favouritedragon.dynamiccombat.skills.fist;
 
+import dynamicswordskills.client.DSSKeyHandler;
+import dynamicswordskills.network.PacketDispatcher;
+import dynamicswordskills.network.bidirectional.ActivateSkillPacket;
+import dynamicswordskills.ref.Config;
 import dynamicswordskills.skills.SkillActive;
 import dynamicswordskills.skills.SkillBase;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class Flurry extends SkillFists {
 
@@ -50,5 +58,26 @@ public class Flurry extends SkillFists {
 	@Override
 	public SkillBase newInstance() {
 		return null;
+	}
+
+	@Override
+	protected void levelUp(EntityPlayer player) {
+		super.levelUp(player);
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public boolean isKeyListener(Minecraft mc, KeyBinding key) {
+		return (key == DSSKeyHandler.keys[DSSKeyHandler.KEY_ATTACK] || (Config.allowVanillaControls() && key == mc.gameSettings.keyBindAttack));
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public boolean keyPressed(Minecraft mc, KeyBinding key, EntityPlayer player) {
+		if (canExecute(player)) {
+			PacketDispatcher.sendToServer(new ActivateSkillPacket(this));
+			return true;
+		}
+		return false;
 	}
 }
